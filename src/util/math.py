1d8674f98ec954dcdd1fa4d883f225c382b21a84
Allen Glenn Castillo <castilloglenn@ymail.com>
2023-07-28 23:17:43 +0800

    refactor: migrate to motion.py

--- src/util/math.py
+++ src/util/math.py
@@ -4,33 +4,7 @@ from absl import flags
 from pygame import Rect, Vector2
 from pygame.sprite import Sprite
 
-from src.util.state import ActionState
-
 FLAGS = flags.FLAGS
-
-
-def get_normalized_movement(
-    action_state: ActionState, speed: int, speed_ampify: float, delta: float
-) -> Vector2:
-    movement_vector = Vector2(0, 0)
-
-    # # Vertical movement is not yet supported
-    # if action_state.move_up and not action_state.move_down:
-    #     movement_vector.y -= 1
-    # elif action_state.move_down and not action_state.move_up:
-    #     movement_vector.y += 1
-
-    if action_state.move_left and not action_state.move_right:
-        movement_vector.x -= 1
-    elif action_state.move_right and not action_state.move_left:
-        movement_vector.x += 1
-
-    if movement_vector.length() > 0:
-        normalized_movement_vector = movement_vector.normalize()
-    else:
-        normalized_movement_vector = movement_vector
-
-    return normalized_movement_vector * (speed * speed_ampify) * delta
 
 
 def add_vector_to_rect(rect: Rect, vector: Vector2):
