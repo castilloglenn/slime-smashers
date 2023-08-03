c1addb94130ba289d75776421612e12888999c31
Allen Glenn Castillo <castilloglenn@ymail.com>
2023-08-04 03:14:12 +0800

    refactor: signify quantity

--- src/cluster/player.py
+++ src/cluster/player.py
@@ -32,12 +32,13 @@ class Player(Sprite):
         self.status_effects = set()
 
         self.action = ActionState()
-        self.animation = Spritesheet(spritesheet=sheet)
+        self.animations = Spritesheet(spritesheet=sheet)
         self.bound = Bound(
-            source=self.animation.idle_sprite,
+            source=self.animations.idle_sprite,
             window=WindowRelPos(rel_x, 0.0),
             hitbox=HitboxRelPos(0.44, 0.45, 0.125, 0.22),
         )
+
         self.motion = Motion(gravity_=800, ms=450)
         self.jump = JumpSequence(duration=0.7, length=200)
         self.dash = DashSequence(speed=1_000, distance=150)
@@ -63,24 +64,24 @@ class Player(Sprite):
                 if not self.jump.is_jumping:
                     self.jump.start(player_rect=self.rect)
 
-        if not self.animation.is_performing:
+        if not self.animations.is_performing:
             if is_new_only(old=self.action, new=actions, attr="attack"):
                 # NOTE Attack starting point
-                self.animation.update_perf(new="attack")
+                self.animations.update_perf(new="attack")
             elif actions.defend:
                 # NOTE Defend starting point
-                self.animation.update_perf(new="defend")
+                self.animations.update_perf(new="defend")
                 self.motion.modify_ms(n=0.25)
 
         if is_old_only(old=self.action, new=actions, attr="defend"):
             # NOTE Defend ending point
-            self.animation.reset_perf()
+            self.animations.reset_perf()
             self.motion.modify_ms(n=1.0)
 
         if is_new_only(old=self.action, new=actions, attr="dash"):
             if not self.dash.is_dashing:
                 self.add_status_effects(self.dash.status_effects)
-                self.animation.update_perf(new="dash")
+                self.animations.update_perf(new="dash")
                 self.dash.start()
                 if actions.move_right:
                     self.dash.direction = DashSequence.RIGHT
@@ -93,7 +94,7 @@ class Player(Sprite):
 
     def apply_movement(self, delta: float, collisions: list[Sprite]):
         if self.action.is_moving:
-            self.animation.hz_flip = not self.motion.is_facing_right
+            self.animations.hz_flip = not self.motion.is_facing_right
 
             new_rect = self.rect.copy()
             movement = self.motion.get_move(delta=delta, action_state=self.action)
@@ -108,13 +109,13 @@ class Player(Sprite):
         self.jump.update(player_rect=self.rect, delta=delta, collisions=collisions)
         self.bound.align_rects()
 
-        if not self.animation.is_performing:
+        if not self.animations.is_performing:
             if self.jump.status == JumpSequence.RISING:
-                self.animation.update_idle(new="rise")
+                self.animations.update_idle(new="rise")
             elif self.jump.status == JumpSequence.FALLING:
-                self.animation.update_idle(new="fall")
+                self.animations.update_idle(new="fall")
             else:
-                self.animation.update_idle(new="idle")
+                self.animations.update_idle(new="idle")
 
     def apply_dash(self, delta: float, collisions: list[Sprite]):
         self.dash.update(player_rect=self.rect, delta=delta, collisions=collisions)
@@ -122,7 +123,7 @@ class Player(Sprite):
 
         if not self.dash.is_dashing:
             self.del_status_effects(self.dash.status_effects)
-            self.animation.reset_perf()
+            self.animations.reset_perf()
             self.motion.move_lock = None
 
     def apply_gravity(self, delta: float, collisions: list[Sprite]):
@@ -131,10 +132,10 @@ class Player(Sprite):
 
         if collision := get_collided(rect=self.rect, collisions=collisions):
             place_rect_on_top(top=self.rect, bottom=collision.rect)
-            self.animation.update_idle(new="idle")
+            self.animations.update_idle(new="idle")
             self.motion.on_ground = True
         else:
-            self.animation.update_idle(new="fall")
+            self.animations.update_idle(new="fall")
             self.motion.on_ground = False
 
         self.bound.align_rects()
@@ -155,13 +156,13 @@ class Player(Sprite):
         else:
             self.apply_gravity(delta=delta, collisions=collisions)
 
-        self.animation.update(delta=delta)
+        self.animations.update(delta=delta)
 
     def draw(self, surface: Surface):
-        sprite = self.animation.get_sprite()
+        sprite = self.animations.get_sprite()
         sprite.draw(
             surface=surface,
             topleft=self.bound.image_start,
-            hz_flip=self.animation.hz_flip,
+            hz_flip=self.animations.hz_flip,
         )
         self.bound.draw(surface=surface)
