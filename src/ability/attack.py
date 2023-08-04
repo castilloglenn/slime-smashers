ec2a096a00dc676573212c1125788d564e518659
Allen Glenn Castillo <castilloglenn@ymail.com>
2023-08-04 09:24:52 +0800

    refactor: change hit to red, miss to white

--- src/ability/attack.py
+++ src/ability/attack.py
@@ -59,9 +59,9 @@ class AttackSequence:
     @property
     def color(self) -> Color:
         if self.strike_status == AttackSequence.MISSED:
-            return Color(255, 0, 0, 128)
+            return Color(255, 255, 255, 128)
         elif self.strike_status == AttackSequence.HIT:
-            return Color(255, 255, 0, 128)
+            return Color(255, 0, 0, 128)
 
     def start(self):
         self.status = AttackSequence.WINDUP
