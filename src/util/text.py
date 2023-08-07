21fcb41711d1881df64f436c1b69059ab3ed19bf
Allen Glenn Castillo <castilloglenn@ymail.com>
2023-08-07 15:07:15 +0800

    fix: kwargs unspecification

--- src/util/text.py
+++ src/util/text.py
@@ -6,11 +6,11 @@ from pygame.surface import Surface
 FLAGS = flags.FLAGS
 
 
-def get_font(font_name: str, font_size: int) -> Font:
-    return Font(name=FLAGS.game.path.ttf[font_name], size=font_size)
+def get_font(name: str, size: int) -> Font:
+    return Font(FLAGS.game.path.ttf[name], size)
 
 
 def get_bitmap(
     font: Font, text: str, antialias: bool = True, color: Color = (0, 0, 0)
 ) -> Surface:
-    return font.render(text=text, antialias=antialias, color=color)
+    return font.render(text, antialias, color)
