f9dd7ed98946976bbf0c53118139d5118fef5649
Allen Glenn Castillo <castilloglenn@ymail.com>
2023-08-09 14:48:31 +0800

    feat: log controller and keyboard actions

--- src/test.py
+++ src/test.py
@@ -118,9 +118,11 @@ class TestEnvironment:
                     joysticks=joysticks, joy_id=p2_joy_id
                 )
                 player_1.receive_actions(actions=controller_actions)
+                text_logger.absorb_actions(name="Joystick", actions=controller_actions)
 
             keyboard_actions = map_keyboard_action()
             player_2.receive_actions(actions=keyboard_actions)
+            text_logger.absorb_actions(name="Keyboard", actions=keyboard_actions)
 
             player_1.update(delta=delta, collisions=p1_collisions)
             player_2.update(delta=delta, collisions=p2_collisions)
